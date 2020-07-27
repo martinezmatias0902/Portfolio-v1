3eee97942ead80abfe9ee141a291b204b3b65e50
martinezmatias0902 <martinez.matias0902@gmail.com>
2020-07-26 22:15:50 -0300

    button go up

--- src/config.js
+++
@@ -1,5 +0,0 @@
-
-
-
-
-let openToWork = (me, recruiter) => 
--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", async() => {
     const skills = document.querySelector(".third");
     const proyectos = document.querySelector(".fourth");
     const contacto = document.querySelector(".fifth");
+    const goUp = document.querySelector(".goUp")
 
     const check = document.querySelector("#check")
 
@@ -22,5 +23,25 @@ document.addEventListener("DOMContentLoaded", async() => {
     contacto.addEventListener('click', () => {
         check.click();
     })
+
+
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goUp.style.display = "block";
+        } else {
+            goUp.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+
+    goUp.addEventListener('click', () => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    })
+
     
 });
